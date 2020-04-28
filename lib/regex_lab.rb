bfcf73d6ad3ba7e2b7abb44300326853eda0cada
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-28 20:57:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match ? true : false
+  phone.match(/^(?:\([2-9]\d{2}\)\ ?|[2-9]\d{2}(?:\-?|\ ?))[2-9]\d{2}[- ]?\d{4}$/) ? true : false
 end
