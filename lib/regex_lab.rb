5511f805788560b9918b70f8592fc3434ccbd1ec
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-28 18:09:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\b[aeyiuo:AEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  b = []
+  text.split(" ").collect {|word| word.match(/un/) && word.match(/ing/) ? b << word : nil}
+  b
 end
 
 def words_five_letters_long(text)
